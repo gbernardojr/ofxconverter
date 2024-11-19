bbafdce1b95c867546c83f67d179869073ec49ee
Gilberto Aparecido Bernardo Junior <36751006+gbernardojr@users.noreply.github.com>
2024-11-19 16:52:00 -0300

    Update converter.py

--- converter.py
+++ converter.py
@@ -187,8 +187,7 @@ st.markdown("**Chave PIX:** gilberto@gbernardoti.com.br")
 st.markdown("---")
 st.markdown("""
 #### Desenvolvedores:
-**Gilberto Aparecido Bernardo Junior** 
-**Gabrielli Letícia**  
+**Gilberto Aparecido Bernardo Junior | Gabrielli Letícia**  
 
 **WhatsApp:** +55 (16) 9.8857-2758  
 **E-mail:** [gilberto@gbernardoti.com.br](mailto:gilberto@gbernardoti.com.br)  
